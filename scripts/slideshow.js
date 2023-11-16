3d11d7722cb102b87b56623c3b02f857f95a7ba8
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-16 21:27:42 +0200

    fix slideshow

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -3,45 +3,71 @@ let slideIndex2 = 0;
 let slides = document.getElementsByClassName("mySlides");
 let dots = document.getElementsByClassName("dot");
 
-// Next/previous controls
+// Function to show next/previous slides
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// Function to show a specific slide
 function currentSlide(n) {
   showSlides(slideIndex = n);
 } 
 
+// Function to control the display of slides
 function showSlides(n) {
   let i;
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  
+  // Check for boundaries
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  // Remove 'active' class from all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // Display the current slide and mark the corresponding dot as 'active'
   slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
 }
 
-
+// Function to automatically show slides
 function showSlides2() {
   let i;
+
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+
+  // Move to the next slide
   slideIndex2++;
-  if (slideIndex2 > slides.length) {slideIndex2 = 1}    
+  if (slideIndex2 > slides.length) {
+    slideIndex2 = 1;
+  }    
+
+  // Remove 'active' class from all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // Display the current slide and mark the corresponding dot as 'active'
   slides[slideIndex2-1].style.display = "flex";  
   dots[slideIndex2-1].className += " active";
-  setTimeout(showSlides2, 5000);
+
+  // Set a timeout for the next iteration
+  setTimeout(showSlides2, 2000);
 }
 
+// Initial calls to display slides
 showSlides(slideIndex);
 showSlides2();
--- scripts/slideshowtext.js
+++ scripts/slideshowtext.js
@@ -1,23 +1,30 @@
+// Configure Typed.js
 var typed = new Typed('#typed', {
-    strings: ['Software engineer?', 'Software engineer?', 'Data engineer?', 'UI/UX designer?'],
-    typeSpeed: 50,
-    backSpeed: 45,
-    bindInputFocusEvents: true,
-    loop: true,
-    cursorChar: '_',
-    backDelay: 1200,
-    startDelay: 0,
-    preStringTyped: (arrayPos, self) => {
-       let colors = [
-          'rgba(40, 238, 167, 0.93)',
-          'rgba(40, 238, 167, 0.93)',
-          'rgba(152, 100, 218, 0.93)',
-          'rgba(251, 80, 142, 0.93)',
-       ];
-       let textColors = ['#363738', '#363738', '#ffffff', '#ffffff'];
-       document.getElementById('typed_container').style.backgroundColor = colors[arrayPos];
-       document.getElementById('typed').style.color = textColors[arrayPos];
-    }
-  });
-
-  
+   strings: ['Software engineer?', 'Software engineer?', 'Data engineer?', 'UI/UX designer?'],
+   typeSpeed: 50,
+   backSpeed: 45,
+   bindInputFocusEvents: true,
+   loop: true,
+   cursorChar: '_',
+   backDelay: 1200,
+   startDelay: 0,
+   // Callback before each string is typed
+   preStringTyped: (arrayPos, self) => {
+     // Define background colors for each position
+     let backgroundColors = [
+       'rgba(40, 238, 167, 0.93)',
+       'rgba(40, 238, 167, 0.93)',
+       'rgba(152, 100, 218, 0.93)',
+       'rgba(251, 80, 142, 0.93)',
+     ];
+     // Define text colors for each position
+     let textColors = ['#363738', '#363738', '#ffffff', '#ffffff'];
+     
+     // Update background color based on the current position
+     document.getElementById('typed_container').style.backgroundColor = backgroundColors[arrayPos];
+     
+     // Update text color based on the current position
+     document.getElementById('typed').style.color = textColors[arrayPos];
+   }
+ });
+ 
